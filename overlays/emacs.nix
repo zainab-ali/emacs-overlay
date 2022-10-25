6cfe47315ffccf68916b3b28b594d08e7706d230
Zainab Ali <zainabali@C02ZQBZYMD6T.intenthq.com>
2022-10-25 23:07:16 +0100

    Use install_name_tool as part of tree-sitter-grammars

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -75,23 +75,18 @@ let
             libName = drv: super.lib.removeSuffix "-grammar" drv.pname;
             ext = if self.stdenv.isDarwin then "dylib" else "so";
             lib = drv: ''lib${libName drv}.${ext}'';
-            linkCmd = drv: "ln -s ${drv}/parser $out/lib/${lib drv}";
-            linkerFlag = drv: "-l" + libName drv;
-
-            newDrv = drv: drv.overrideAttrs (old: {
-              # makeFlags = "LDFLAGS=-Wl,-install_name,$(out)/lib/${lib old}";
-              fixupPhase = ''
-               install_name_tool -id $out/lib/${lib old} $out/parser
-               mkdir -p $out/lib
-               ln -s $out/parser $out/lib/${lib old}
+            linkCmd = drv: if !self.stdenv.isDarwin then "ln -s ${drv}/parser $out/lib/${lib drv}"
+                           else ''
+ln -s ${drv}/parser $out/lib/${lib drv}
+install_name_tool -id $out/lib/${lib drv} $out/lib/${lib drv};
 '';
-            });
+            linkerFlag = drv: "-l" + libName drv;
 
             plugins = args.withTreeSitterPlugins self.pkgs.tree-sitter-grammars;
             tree-sitter-grammars = super.runCommand "tree-sitter-grammars" {}
-              (super.lib.concatStringsSep "\n" (["mkdir -p $out/lib"] ++ (map linkCmd (map newDrv plugins))));
+              (super.lib.concatStringsSep "\n" (["mkdir -p $out/lib"] ++ (map linkCmd plugins)));
           in {
-            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter ] ++ (map newDrv plugins);
+            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter tree-sitter-grammars ];
             # before building the `.el` files, we need to allow the `tree-sitter` libraries
             # bundled in emacs to be dynamically loaded.
             TREE_SITTER_LIBS = super.lib.concatStringsSep " " ([ "-ltree-sitter" ] ++ (map linkerFlag plugins)); 
