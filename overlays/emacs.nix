3fb175b7bebe646b715fb93975b1450f09c3f03a
Zainab Ali <zainabali@C02ZQBZYMD6T.intenthq.com>
2022-10-25 17:35:13 +0100

    Both

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -86,7 +86,7 @@ let
             tree-sitter-grammars = super.runCommand "tree-sitter-grammars" {}
               (super.lib.concatStringsSep "\n" (["mkdir -p $out/lib"] ++ (map linkCmd (map newDrv plugins))));
           in {
-            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter tree-sitter-grammars];
+            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter tree-sitter-grammars] ++ (map newDrv plugins);
             # before building the `.el` files, we need to allow the `tree-sitter` libraries
             # bundled in emacs to be dynamically loaded.
             TREE_SITTER_LIBS = super.lib.concatStringsSep " " ([ "-ltree-sitter" ] ++ (map linkerFlag plugins)); 
