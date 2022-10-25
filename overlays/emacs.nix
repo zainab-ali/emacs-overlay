5a69b8f8b2526e454c10cc022d12eba6aebb685e
Zainab Ali <zainabali@C02ZQBZYMD6T.intenthq.com>
2022-10-25 15:58:07 +0100

    Add linker flags

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -77,11 +77,16 @@ let
             lib = drv: ''lib${libName drv}.${ext}'';
             linkCmd = drv: "ln -s ${drv}/parser $out/lib/${lib drv}";
             linkerFlag = drv: "-l" + libName drv;
+
+            newDrv = drv: drv.overrideAttrs (old: {
+              makeFlags = "LDFLAGS=-Wl,-install_name,$(out)/lib/${lib old}";
+            });
+
             plugins = args.withTreeSitterPlugins self.pkgs.tree-sitter-grammars;
             tree-sitter-grammars = super.runCommand "tree-sitter-grammars" {}
               (super.lib.concatStringsSep "\n" (["mkdir -p $out/lib"] ++ (map linkCmd plugins)));
           in {
-            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter tree-sitter-grammars ];
+            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter] ++ (map newDrv plugins);
             # before building the `.el` files, we need to allow the `tree-sitter` libraries
             # bundled in emacs to be dynamically loaded.
             TREE_SITTER_LIBS = super.lib.concatStringsSep " " ([ "-ltree-sitter" ] ++ (map linkerFlag plugins)); 
