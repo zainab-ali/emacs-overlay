4ff52af31a6970b4b7aacab2fe37209c6959b925
Zainab Ali <zainabali@C02ZQBZYMD6T.intenthq.com>
2022-10-25 17:52:48 +0100

    Use fixup instead

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -79,14 +79,19 @@ let
             linkerFlag = drv: "-l" + libName drv;
 
             newDrv = drv: drv.overrideAttrs (old: {
-              makeFlags = "LDFLAGS=-Wl,-install_name,$(out)/lib/${lib old}";
+              # makeFlags = "LDFLAGS=-Wl,-install_name,$(out)/lib/${lib old}";
+              fixupPhase = ''
+               install_name_tool -id $out/lib/${lib old} $out/parser
+               mkdir -p $out/lib
+               ln -s $out/parser $out/lib/${lib old}
+'';
             });
 
             plugins = args.withTreeSitterPlugins self.pkgs.tree-sitter-grammars;
             tree-sitter-grammars = super.runCommand "tree-sitter-grammars" {}
               (super.lib.concatStringsSep "\n" (["mkdir -p $out/lib"] ++ (map linkCmd (map newDrv plugins))));
           in {
-            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter tree-sitter-grammars] ++ (map newDrv plugins);
+            buildInputs = old.buildInputs ++ [ self.pkgs.tree-sitter ] ++ (map newDrv plugins);
             # before building the `.el` files, we need to allow the `tree-sitter` libraries
             # bundled in emacs to be dynamically loaded.
             TREE_SITTER_LIBS = super.lib.concatStringsSep " " ([ "-ltree-sitter" ] ++ (map linkerFlag plugins)); 
